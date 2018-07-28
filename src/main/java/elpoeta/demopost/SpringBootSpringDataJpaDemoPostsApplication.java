84261833e619b8556c939d1a619033c79154aa24
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-07-28 23:09:14 +0000

    primer commit genearacion proyecto spring boot

---
+++ src/main/java/elpoeta/demopost/SpringBootSpringDataJpaDemoPostsApplication.java
@@ -0,0 +1,12 @@
+package elpoeta.demopost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringDataJpaDemoPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataJpaDemoPostsApplication.class, args);
+	}
+}
---
+++ src/test/java/elpoeta/demopost/SpringBootSpringDataJpaDemoPostsApplicationTests.java
@@ -0,0 +1,16 @@
+package elpoeta.demopost;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSpringDataJpaDemoPostsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
